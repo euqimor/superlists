417ebbf228a10d9e32814f8b2ba0d120af5ab297
euqimor <romique@gmail.com>
2017-01-23 22:21:58 +0300

    A commit prior to refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Dracula has heard about a new online to-do list app. He opens his browser and enters the url to check it out.
         self.browser.get('http://localhost:8000')
@@ -30,9 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         #"1: Buy fresh virgin blood online".
         inputBox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy fresh virgin blood online', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy fresh virgin blood online')
 
         #There's still a text box inviting him to enter another item.
         #He enters "Sharpen the teeth".
@@ -41,10 +44,8 @@ class NewVisitorTest(unittest.TestCase):
         inputBox.send_keys(Keys.ENTER)
 
         #The page updates again and now shows both items on the list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy fresh virgin blood online', [row.text for row in rows])
-        self.assertIn('2: Sharpen the teeth', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy fresh virgin blood online')
+        self.check_for_row_in_list_table('2: Sharpen the teeth')
 
         #Dracula is concerned if the site will remember his to-do list. Then he notices that the site has generated
         #a unique URL for him. There's some text explaining that on the page.
