c5fccea714c5721599089e4435c6463a070dfb41
euqimor <romique@gmail.com>
2017-01-25 00:49:12 +0300

    Template now shows all entries, added POST redirect and corresponding tests

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 from django.views.decorators.csrf import csrf_exempt #delete when possible
 
 @csrf_exempt #delete when possible
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
+    items = Item.objects.all()
     return render(request, 'home.html', {
-        'new_item_text': new_item_text,
+        'items': items
     })
